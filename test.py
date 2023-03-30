f9e6cb1ef43adc72e1acd9c61fe10b71134827aa
ignacio-fernandez <ifndez3@gmail.com>
2023-03-30 11:01:21 -0400

    delete files

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-import pandas as pd
-
-engine = sqlalchemy.create_engine("mysql+pymysql://" + 'root' + ":" + 'Elderscro11s!' + "@" + 'localhost' + "/" + 'foo')
-
-df = pd.DataFrame({'id': [1, 2, 3], 'nullable': ['blah', None, 'bl']})
-data_types = {'id': sqlalchemy.types.INTEGER(), 'nullable': sqlalchemy.types.NVARCHAR(5)}
-
-df.to_sql('test', engine, if_exists='replace', index=False, dtype=data_types)
